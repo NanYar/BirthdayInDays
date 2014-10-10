4d570a3c70e60c3637263ef55717fb2936e2c01e
NanYar <github@sagner.org>
2014-10-11 00:23:01 +0200

    Connected the user interface to swift code

--- BirthdayInDays/ViewController.swift
+++ BirthdayInDays/ViewController.swift
@@ -9,17 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var dayTextField: UITextField!
+    @IBOutlet weak var monthTextField: UITextField!
+    @IBOutlet weak var yearTextField: UITextField!
+    @IBOutlet weak var messageLabel: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
+    @IBAction func daysButtonPressed(sender: UIButton) {
+        
+        messageLabel.text = "I am xyz days old"
+        messageLabel.hidden = false
+        dayTextField.resignFirstResponder()
+        monthTextField.resignFirstResponder()
+        yearTextField.resignFirstResponder()
+        
+    }
+    
 }
-
